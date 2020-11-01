119b64c70b99475666b55948a4d3c184cbb02b3b
Igor2344 <Igor2344@users.noreply.github.com>
2020-11-01 12:51:52 -0800

    Update GoogleFoobar3.667.py
    Finished Google foobar 3.66

--- Python/Foobar/GoogleFoobar3.667.py
+++ Python/Foobar/GoogleFoobar3.667.py
@@ -5,30 +5,49 @@ Created on Sat Oct 31 22:44:47 2020
 @author: Toast
 """
 
-def solution(n):
+def oldSolution(n):
   n = long(n)
-  x = long(1)
-  steps = 0
-  while(x < n):
-    x *= 2
-    steps += 1
-  #check if x is within 1 of n
-  if n + 1 == x or n - 1 == x/2:
-    return steps + 1
-  else:
-    if n % 2 == 0:
-      n = n/2
-      x = x/2
-      steps -= 1
-      if n + 1 == x or n - 1 == x:
-        return steps + 1
-      else:
-        steps = steps + solution(n)
+  x = [1L]
+  step = 0
+  while x[len(x)-1] < n:
+    x.append(x[len(x)-1] * 2)
+  while len(x) > 2:
+    if n == x[len(x)-1] or n - 1 == x[len(x)-2]:
+      return len(x)-1 + step
+    elif n + 1 == x[len(x)-1]:
+      return len(x) + step
     else:
-      n = (n-1)/2
-      x = x/2
-      steps = (steps - 1) + solution(n)
-  return steps
+      x.pop()
+      if n % 2 == 0:
+        n = n / 2
+        step += 1
+      else: # decide whether to add 1 or sub 1
+        if (n+1)&n > (n-1)&(n-2):
+          n = (n - 1) / 2
+          step += 2
+        else:
+          n = (n + 1) / 2
+          step += 2
+  # n is 3L, 2L, or 1L, return 2 1 or 0
+  return int(n) - 1 + step
 
-  if __name__ == "__main__":
-    print(solution(10))
+def test2():
+  for x in range(1L,100L):
+    if oldSolution(x) != solution(x):
+      print ("False",x)
+
+def solution(n):
+    n = long(n)
+    steps = 0
+    while(n != 1):
+        if n % 2 == 0:
+            n /= 2
+        elif n == 3 or (n+1) & n > ((n-1) & (n-2)):
+            n -= 1
+        else:
+            n += 1
+        steps += 1
+    return steps
+
+if __name__ == "__main__":
+  test2()
